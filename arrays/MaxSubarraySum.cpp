7ab269266d4af0ed4b452038d6f9d6da31e57793
Alex Moskalenko <msumoskalenko@gmail.com>
2017-06-25 21:13:21 +0300

    Max Sum Contiguous Subarray

---
+++ arrays/MaxSubarraySum.cpp
@@ -0,0 +1,32 @@
+/*
+Max Sum Contiguous Subarray
+
+
+Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+
+For example:
+
+Given the array [-2,1,-3,4,-1,2,1,-5,4],
+
+the contiguous subarray [4,-1,2,1] has the largest sum = 6.
+
+For this problem, return the maximum sum.
+*/
+
+int Solution::maxSubArray(const vector<int> &A) { // O(N^2)
+   int max = INT_MIN, n = A.size();
+   for (int i = 0; i < n; i++){ // start index
+       int sum = 0;
+       for (int sz = 1; sz <= n; sz++){ // size of sequence
+           if (sz + i > n){
+               break;
+           }
+           sum += A[i + sz - 1];
+           if (sum >= max){
+               max = sum;
+           }
+       }
+   }
+   
+   return max;
+}
