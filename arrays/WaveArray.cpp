72561418376752847deb25e5f1ff42cc8dba03e0
Alex Moskalenko <msumoskalenko@gmail.com>
2017-06-30 00:23:13 +0300

    a1 >= a2 <= a3 >= a4 <= a5.....

---
+++ arrays/WaveArray.cpp
@@ -0,0 +1,31 @@
+/*
+
+Given an array of integers, sort the array into a wave like array and return it,
+In other words, arrange the elements into a sequence such that a1 >= a2 <= a3 >= a4 <= a5.....
+
+Example
+
+Given [1, 2, 3, 4]
+
+One possible answer : [2, 1, 4, 3]
+Another possible answer : [4, 1, 3, 2]
+
+    NOTE : If there are multiple answers possible, return the one thats lexicographically smallest.
+    So, in example case, you will return [2, 1, 4, 3] 
+
+
+*/
+
+vector<int> Solution::wave(vector<int> &a) {
+    sort(begin(a), end(a));
+    int n = a.size();
+    if (n == 1){
+        return a;
+    }
+    for (int i = 0; i < n; i++){
+        if (i % 2  == 0 && i + 1 < n){
+            swap(a[i], a[i+1]);
+        }
+    }
+    return a;
+}
