c0e31a200f38851411ad5d332a3f11ff5f470495
Alex Moskalenko <msumoskalenko@gmail.com>
2017-06-30 15:54:21 +0300

    find max of (j - i) if (A[i] <= A[j])

---
+++ arrays/MaxDistanse.cpp
@@ -0,0 +1,37 @@
+/*
+Given an array A of integers, find the maximum of j - i subjected to the constraint of A[i] <= A[j].
+
+If there is no solution possible, return -1.
+
+Example :
+
+A : [3 5 4 2]
+
+Output : 2 
+for the pair (3, 4)
+*/
+
+
+
+class Solution {
+    public:
+        int maximumGap(const vector<int> &num) {
+            if (num.size() == 0) return -1;
+            if (num.size() == 1) return 0;
+            vector<pair<int, int> > toSort; 
+            for (int i = 0; i < num.size(); i++) {
+                toSort.push_back(make_pair(num[i], i));
+            }
+            sort(toSort.begin(), toSort.end());
+            int len = toSort.size();
+            int maxIndex = toSort[len - 1].second;
+            int ans = 0;
+            for (int i = len - 2; i >= 0; i--) {
+                ans = max(ans, maxIndex - toSort[i].second);
+                maxIndex = max(maxIndex, toSort[i].second);
+            }
+            return ans;
+        }
+};
+
+
