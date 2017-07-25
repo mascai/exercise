18143e74ba0f40434817bdf4f14b72a30b13510f
Alex <msumoskalenko@gmail.com>
2017-07-25 23:44:04 +0300

    Высота и Ширина

--- 6_trees/0_base.cpp
+++ 6_trees/0_base.cpp
@@ -52,13 +52,50 @@ void Print(Node *root){
     Print(root->right);
 }
 
+int Height(Node *root){
+    int h1 = 0, h2 = 0;
+    if (root == NULL){
+        return 0;
+    }
+    return 1 + max(Height(root->right), Height(root->left));
+}
+
+//returns width of level @k
+int getWidth(Node *root, int k){
+    if (root == NULL){
+        return NULL;
+    }
+    if (k == 1){
+        return 1;
+    }  else if (k > 1){
+        return getWidth(root->left, k-1) +  getWidth(root->right, k-1);
+        getWidth(root->right, k - 1);
+    }
+
+}
+
+int Width(Node* root){
+    int maxWidth = 0;
+    int curWidth = 0, i = 0;
+    int h = Height(root);
+    for (int i = 1; i < h; i++){
+        curWidth = getWidth(root, i);
+        maxWidth = max(maxWidth, curWidth);
+    }
+    return maxWidth;
+}
+
+
 main() {
     Node *root = NULL;
-    root = InsertNode(root,15);
-	root = InsertNode(root,10);
-	root = InsertNode(root,20);
-	root = InsertNode(root,25);
-	root = InsertNode(root,8);
-    root = InsertNode(root,12);
+    root = InsertNode(root,3);
+    root = InsertNode(root,1);
+	root = InsertNode(root,2);
+	root = InsertNode(root,4);
+	root = InsertNode(root,5);
+    root = InsertNode(root,6);
     Print(root);
+
+    cout << endl << Height(root) << endl;
+    cout << Width(root) << endl;
 }
