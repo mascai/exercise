4b02189c3f37d956d1e649afd7beb455248dee02
Alex <msumoskalenko@gmail.com>
2020-04-11 14:11:22 +0300

    Update ArrayBinarySearch.cpp

--- 3_binary_search/ArrayBinarySearch.cpp
+++ 3_binary_search/ArrayBinarySearch.cpp
@@ -38,3 +38,33 @@ int Solution::searchMatrix(vector<vector<int> > &a, int x) {
     }
     return 0;
 }
+
+
+// Second version of solution
+int getElement(const vector<vector<int> > &A, const int id) {
+    int i = id / A[0].size();
+    int j = id - i * A[0].size();
+    return A[i][j];
+}
+
+int Solution::searchMatrix(vector<vector<int> > &A, int B) {
+    int rows = A.size();
+    if (rows == 0) {
+        return 0;
+    }
+    int cols = A[0].size();
+    int start = 0, end = rows * cols - 1;
+    while (start <= end) {
+        int mid = (start + end) / 2;
+        int cur = getElement(A, mid);
+        if (cur == B) {
+            return 1;
+        } else if (B < cur) {
+            end = mid - 1;
+        } else {
+            start = mid + 1;
+        }
+    }
+    return 0;
+}
+
