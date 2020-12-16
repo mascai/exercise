47b5a232038030d576d0daf489954ef73e0e123f
Alex <msumoskalenko@gmail.com>
2020-12-16 20:47:11 +0300

    Update ValidBinaryTree.cpp

--- 6_trees/ValidBinaryTree.cpp
+++ 6_trees/ValidBinaryTree.cpp
@@ -92,3 +92,36 @@ int Solution::isValidBST(TreeNode* root) {
         return false;
     }
 }
+
+
+// https://leetcode.com/problems/validate-binary-search-tree/solution/
+// Solution 3
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool foo(TreeNode* root, long long l, long long r) {
+        if (root == NULL) {
+            return true;
+        }
+        if (root->val <= l or root->val >= r) {
+            return false;
+        }
+        return foo(root->left, l, root->val) and
+               foo(root->right, root->val, r);
+    }
+    bool isValidBST(TreeNode* root) {
+        long long mmin = numeric_limits<long long>::min();
+        long long mmax = numeric_limits<long long>::max();
+        return foo(root, mmin, mmax);
+    }
+};
