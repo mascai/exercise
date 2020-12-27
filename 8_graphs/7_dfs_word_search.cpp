52cacc730e8d95f86bd4fda443cee048520a13fb
Alex <msumoskalenko@gmail.com>
2020-12-27 13:45:46 +0300

    Update 7_dfs_word_search.cpp

--- 8_graphs/7_dfs_word_search.cpp
+++ 8_graphs/7_dfs_word_search.cpp
@@ -1,4 +1,6 @@
 /*
+https://leetcode.com/problems/word-search/
+
 Given an m x n board and a word, find if the word exists in the grid.
 
 The word can be constructed from letters of sequentially adjacent cells, where "adjacent" cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
