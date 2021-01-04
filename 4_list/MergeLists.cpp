0eba0e4acfd22e60e2fac530abc8e4134482c7d6
Alex <msumoskalenko@gmail.com>
2021-01-04 12:59:02 +0300

    Update MergeLists.cpp

--- 4_list/MergeLists.cpp
+++ 4_list/MergeLists.cpp
@@ -13,6 +13,55 @@ The merged list should be :
 
 
 */
+
+
+
+// Solution 1
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (l1 == NULL and l2 == NULL) {
+            return NULL;
+        }
+        if (l1 == NULL) {
+            return l2;
+        }
+        if (l2 == NULL) {
+            return l1;
+        }
+        ListNode* head;
+        if (l1->val < l2->val) {
+            head = l1;
+            l1 = l1->next;
+        } else {
+            head = l2;
+            l2 = l2->next;
+        }
+        ListNode* cur = head;
+        while (l1 or l2) {
+            if (l1 == NULL) {
+                cur->next = l2;
+                break;
+            }
+            if (l2 == NULL) {
+                cur->next = l1;
+                break;
+            }
+            if (l1->val < l2->val) {
+                cur->next = l1;
+                l1 = l1->next;
+            } else {
+                cur->next = l2;
+                l2 = l2->next;
+            }
+            cur = cur->next;
+        }
+        return head;
+    }
+};
+
+
+// Solution 2
 class Solution {
     public:
         ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
