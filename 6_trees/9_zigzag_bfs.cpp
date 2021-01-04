cf8d97eb0e0b3c4aa723a518c30902e578f1a626
Alex <msumoskalenko@gmail.com>
2021-01-04 14:52:08 +0300

    Update 9_zigzag_bfs.cpp

--- 6_trees/9_zigzag_bfs.cpp
+++ 6_trees/9_zigzag_bfs.cpp
@@ -30,6 +30,18 @@ return its zigzag level order traversal as:
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
@@ -41,18 +53,18 @@ public:
         int direction = 1;
         q.push(root);
         while (not q.empty()) {
-            queue<TreeNode*> newq;
             vector<int> level;
-            while (not q.empty()) {
+            int n = q.size();
+            for (int i = 0; i < n; ++i) {
                 TreeNode* temp = q.front();
                 level.push_back(temp->val);
                 q.pop();
                 
                 if (temp->right) {
-                    newq.push(temp->right);
+                    q.push(temp->right);
                 }
                 if (temp->left) {
-                    newq.push(temp->left);
+                    q.push(temp->left);
                 }
             }
             if (direction) {
@@ -60,7 +72,6 @@ public:
             }
             direction ^= 1;
             res.push_back(level);
-            q = std::move(newq);
         }
         return res;
     }
