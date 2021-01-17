6df91c3b81dd95ff0879fb762a3e367c39143d9e
Alex <msumoskalenko@gmail.com>
2021-01-17 18:24:48 +0300

    Create 16_sorted_vowel.cpp

---
+++ 7_dynamic_programming/16_sorted_vowel.cpp
@@ -0,0 +1,41 @@
+/*
+Given an integer n, return the number of strings of length n that consist only of vowels (a, e, i, o, u) and are lexicographically sorted.
+
+A string s is lexicographically sorted if for all valid i, s[i] is the same as or comes before s[i+1] in the alphabet.
+
+ 
+
+Example 1:
+
+Input: n = 1
+Output: 5
+Explanation: The 5 sorted strings that consist of vowels only are ["a","e","i","o","u"].
+Example 2:
+
+Input: n = 2
+Output: 15
+Explanation: The 15 sorted strings that consist of vowels only are
+["aa","ae","ai","ao","au","ee","ei","eo","eu","ii","io","iu","oo","ou","uu"].
+Note that "ea" is not a valid string since 'e' comes after 'a' in the alphabet.
+Example 3:
+
+Input: n = 33
+Output: 66045
+*/
+
+class Solution {
+public:
+    int countVowelStrings(int n) {
+        vector<vector<int>> dp(n+1, vector<int>(6, 0));
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= 5; ++j) {
+                if (i == 1) {
+                    dp[i][j] = dp[i][j - 1] + 1;
+                } else {
+                    dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][5];
+    }
+};
