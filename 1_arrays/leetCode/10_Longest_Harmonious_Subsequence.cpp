2eb0fc314f0c0d718e1a43e33e41dc8c1097ba19
Alex <msumoskalenko@gmail.com>
2021-02-04 22:53:59 +0300

    Create 10_Longest_Harmonious_Subsequence.cpp

---
+++ 1_arrays/leetCode/10_Longest_Harmonious_Subsequence.cpp
@@ -0,0 +1,52 @@
+/*
+We define a harmonious array as an array where the difference between its maximum value and its minimum value is exactly 1.
+
+Given an integer array nums, return the length of its longest harmonious subsequence among all its possible subsequences.
+
+A subsequence of array is a sequence that can be derived from the array by deleting some or no elements without changing the order of the remaining elements.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,2,2,5,2,3,7]
+Output: 5
+Explanation: The longest harmonious subsequence is [3,2,2,2,3].
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: 2
+Example 3:
+
+Input: nums = [1,1,1,1]
+Output: 0
+ 
+
+Constraints:
+
+1 <= nums.length <= 2 * 104
+-109 <= nums[i] <= 109
+
+*/
+
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        int res = 0, cur = 0;
+        unordered_map<int, int> cnt; 
+        for (auto i : nums) {
+            cnt[i]++;
+        }
+        for (int i = 0; i < nums.size(); ++i) {
+            int parents = cnt[nums[i] - 1];
+            if (parents > 0) {
+                res = max(res, parents + cnt[nums[i]]);
+            }
+            parents = cnt[nums[i] + 1];
+            if (parents > 0) {
+                res = max(res, parents + cnt[nums[i]]);
+            }
+        }
+        return res;
+    }
+};
