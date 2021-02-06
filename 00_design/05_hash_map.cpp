95df72e5a21a8330d3719a09848fd3a25baa3f6f
Alex <msumoskalenko@gmail.com>
2021-02-06 12:52:35 +0300

    Update 05_hash_map.cpp

--- 00_design/05_hash_map.cpp
+++ 00_design/05_hash_map.cpp
@@ -29,6 +29,42 @@ Please do not use the built-in HashMap library.
 */
 
 
+
+// Solution 1
+class MyHashMap {
+public:
+    /** Initialize your data structure here. */
+    MyHashMap() {
+        memset(data_, -1, 1000001); // -1 -- means that we haven't value for the index
+    }
+    
+    /** value will always be non-negative. */
+    void put(int key, int value) {
+        data_[key] = value;
+    }
+    
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    int get(int key) {
+        return data_[key];
+    }
+    
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    void remove(int key) {
+        data_[key] = -1;
+    }
+private:
+    int data_[1000001];
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
+
+// Solution 2
 class MyHashMap {
 public:
     /** Initialize your data structure here. */
