323c5a281ad86ccbc65a41b4691eaf292fc8719d
Alex <msumoskalenko@gmail.com>
2021-02-07 14:06:58 +0300

    Create 13_shortest_subarr_with_sum_K.cpp

---
+++ 1_arrays/leetCode/13_shortest_subarr_with_sum_K.cpp
@@ -0,0 +1,59 @@
+/*
+Return the length of the shortest, non-empty, contiguous subarray of A with sum at least K.
+
+If there is no non-empty subarray with sum at least K, return -1.
+
+ 
+
+Example 1:
+
+Input: A = [1], K = 1
+Output: 1
+Example 2:
+
+Input: A = [1,2], K = 4
+Output: -1
+Example 3:
+
+Input: A = [2,-1,2], K = 3
+Output: 3
+ 
+
+Note:
+
+1 <= A.length <= 50000
+-10 ^ 5 <= A[i] <= 10 ^ 5
+1 <= K <= 10 ^ 9
+
+*/
+
+
+class Solution {
+public:
+    int shortestSubarray(vector<int>& A, int K) {
+        vector<int> pref(A.size() + 1, 0);
+        int maxVal = INT_MIN;
+        long long total = 0;
+        for (int i = 1; i < A.size() + 1; ++i) {
+            pref[i] = pref[i - 1] + A[i - 1]; 
+            maxVal = max(maxVal, A[i - 1]);
+            tota
+        }
+        if (maxVal >= K) {
+            return 1;
+        }
+        int res = -1;
+
+        for (int i = 0; i < A.size() + 1; ++i) {
+            for (int j = i; j < A.size() + 1; ++j) {
+                if (pref[j] - pref[i] >= K) {
+                    if (res == -1 or res > j - i) {
+                        res = j - i;
+                        break;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
