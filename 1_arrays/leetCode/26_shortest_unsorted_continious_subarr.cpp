d7079a1dbe75f3b322b6556d5dd9917e43ac29ac
Alex <msumoskalenko@gmail.com>
2021-02-25 23:52:40 +0300

    Create 26_shortest_unsorted_continious_subarr.cpp

---
+++ 1_arrays/leetCode/26_shortest_unsorted_continious_subarr.cpp
@@ -0,0 +1,54 @@
+/* https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/587/week-4-february-22nd-february-28th/3652/
+Given an integer array nums, you need to find one continuous subarray that if you only sort this subarray in ascending order, then the whole array will be sorted in ascending order.
+
+Return the shortest such subarray and output its length.
+
+ 
+
+Example 1:
+
+Input: nums = [2,6,4,8,10,9,15]
+Output: 5
+Explanation: You need to sort [6, 4, 8, 10, 9] in ascending order to make the whole array sorted in ascending order.
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: 0
+Example 3:
+
+Input: nums = [1]
+Output: 0
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-105 <= nums[i] <= 105
+*/
+
+
+class Solution {
+public:
+    int findUnsortedSubarray(vector<int>& nums) {
+        int len = nums.size() - 1;
+        int left = -1;
+        int right = -1,
+        int mmax = nums[0], mmin = nums[len];
+        for (int i = 1; i <= len; i++) {
+            int a = nums[i];
+            int b = nums[len - i];
+            if (a < mmax) {
+                right = i;
+            }
+            else { 
+                mmax = a;
+            }
+            if (b > mmin) {
+                left = i;
+            } else {
+                mmin = b;
+            }
+        }
+        return max<int>(0, left + right - len + 1);
+    }
+};
