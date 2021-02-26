abe2a5a0d41cf975f7b6c2a01015e5a296e93b67
Alex <msumoskalenko@gmail.com>
2021-02-26 23:36:06 +0300

    Update 27_validate_stack_sequence.cpp

--- 1_arrays/leetCode/27_validate_stack_sequence.cpp
+++ 1_arrays/leetCode/27_validate_stack_sequence.cpp
@@ -27,7 +27,32 @@ pushed and popped have distinct values.
 
 */
 
+/* O(1) space solution
 
+Please, upvote this solution
+
+The algorithm: (vector ```pushed``` emulates stack, ```pushId``` emulates size of the stack)
+1) At i iteration place pushed[i] at the top of the stask. 
+2) While pushed[pushId] == popped[poppId]. Pop top-element of the stack
+*/
+
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        int pushId = -1, poppId = 0;
+        for (int i = 0; i < pushed.size(); ++i) {
+            pushed[++pushId] = pushed[i];
+            while (pushId >= 0 and pushed[pushId] == popped[poppId]) {
+                --pushId;
+                ++poppId;
+            }
+        }
+        return pushId == -1;
+    }
+};
+
+
+// O(N) space solution
 class Solution {
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
