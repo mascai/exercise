0ffc64e454168594ac7a29ef34e27869d0c69807
Alex <msumoskalenko@gmail.com>
2021-02-28 13:32:15 +0300

    Create 09_max_frequency_stack.cpp

---
+++ 00_design/09_max_frequency_stack.cpp
@@ -0,0 +1,74 @@
+/* https://leetcode.com/submissions/detail/461664480/?from=explore&item_id=3655
+Implement FreqStack, a class which simulates the operation of a stack-like data structure.
+
+FreqStack has two functions:
+
+push(int x), which pushes an integer x onto the stack.
+pop(), which removes and returns the most frequent element in the stack.
+If there is a tie for most frequent element, the element closest to the top of the stack is removed and returned.
+ 
+
+Example 1:
+
+Input: 
+["FreqStack","push","push","push","push","push","push","pop","pop","pop","pop"],
+[[],[5],[7],[5],[7],[4],[5],[],[],[],[]]
+Output: [null,null,null,null,null,null,null,5,7,5,4]
+Explanation:
+After making six .push operations, the stack is [5,7,5,7,4,5] from bottom to top.  Then:
+
+pop() -> returns 5, as 5 is the most frequent.
+The stack becomes [5,7,5,7,4].
+
+pop() -> returns 7, as 5 and 7 is the most frequent, but 7 is closest to the top.
+The stack becomes [5,7,5,4].
+
+pop() -> returns 5.
+The stack becomes [5,7,4].
+
+pop() -> returns 4.
+The stack becomes [5,7].
+ 
+
+Note:
+
+Calls to FreqStack.push(int x) will be such that 0 <= x <= 10^9.
+It is guaranteed that FreqStack.pop() won't be called if the stack has zero elements.
+The total number of FreqStack.push calls will not exceed 10000 in a single test case.
+The total number of FreqStack.pop calls will not exceed 10000 in a single test case.
+The total number of FreqStack.push and FreqStack.pop calls will not exceed 150000 across all test cases.
+
+*/
+
+class FreqStack {
+public:
+    FreqStack() = default;
+    
+    void push(int x) {
+        cnt_[x]++;
+        maxFrequency_ = max(maxFrequency_, cnt_[x]);
+        sts_[cnt_[x]].push(x);
+    }
+    
+    int pop() {
+        int delete_elem = sts_[maxFrequency_].top();
+        sts_[maxFrequency_].pop();
+        if (sts_[maxFrequency_].empty()) {
+            sts_.erase(maxFrequency_);
+            maxFrequency_--;
+        }
+        cnt_[delete_elem]--;
+        return delete_elem;
+    }
+private:
+    unordered_map <int, int> cnt_;
+    unordered_map <int, stack<int> >sts_;
+    int maxFrequency_ = 0;
+};
+
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * FreqStack* obj = new FreqStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ */
