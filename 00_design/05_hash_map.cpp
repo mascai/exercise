72b9b60c1a4af090e8673faa579bb1cb0fff5dca
Alex <msumoskalenko@gmail.com>
2021-03-07 21:53:28 +0300

    Update 05_hash_map.cpp

--- 00_design/05_hash_map.cpp
+++ 00_design/05_hash_map.cpp
@@ -29,6 +29,64 @@ Please do not use the built-in HashMap library.
 */
 
 
+// Solution 0 (better in terms of memory)
+class MyHashMap {
+public:
+    /** Initialize your data structure here. */
+    MyHashMap() {
+        data.resize(size);
+    }
+    
+    int get_hash(int n) {
+        return n % size;
+    }
+    
+    /** value will always be non-negative. */
+    void put(int key, int value) {
+        vector<pair<int, int>>& row = data[get_hash(key)];
+        for (auto& i : row) {
+            if (i.first == key) {
+                i.second = value;
+                return;
+            }
+        }
+        row.push_back({key, value});
+    }
+    
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    int get(int key) {
+        vector<pair<int, int>>& row = data[get_hash(key)];
+        for (auto i : row) {
+            if (i.first == key) {
+                return i.second;
+            }
+        }
+        return -1;
+    }
+    
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    void remove(int key) {
+        vector<pair<int, int>>& row = data[get_hash(key)];
+        for (auto iter = row.begin(); iter != row.end(); ++iter) {
+            if (iter->first == key) {
+                row.erase(iter);
+                return;
+            }
+        }
+    }
+private:
+    static const int size = 1001;
+    vector<vector<pair<int, int>>> data;
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
+
 
 // Solution 1
 class MyHashMap {
