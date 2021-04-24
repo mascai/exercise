be625b2e942eef56b8059fc48bb132529184d0de
Alex <msumoskalenko@gmail.com>
2021-04-24 18:16:27 +0300

    Create 49_two_sums_sorted_array.cpp

---
+++ 1_arrays/leetCode/49_two_sums_sorted_array.cpp
@@ -0,0 +1,21 @@
+/*
+https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l = 0, r = numbers.size() - 1;
+        while (l < r) {
+            if (numbers[l] + numbers[r] == target) {
+                return {l+1, r+1};
+            } 
+            if (numbers[l] + numbers[r] < target) {
+                ++l;
+            } else {
+                --r;
+            }
+        }
+        return {};
+    }
+};
