ccc0664df501990a848467c7e0d240b91f5f98f8
Alex <msumoskalenko@gmail.com>
2022-10-12 01:58:28 +0300

    Update 18_longest_sequence.cpp

--- 7_dynamic_programming/18_longest_sequence.cpp
+++ 7_dynamic_programming/18_longest_sequence.cpp
@@ -24,4 +24,27 @@ public:
     }
 };
 
+
 // Solution 2 O(N^2)
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<vector<int>> dp(nums.size(), vector<int>(nums.size() + 1, -1));
+        return max_len(dp, nums, 0, -1);
+    }
+    
+    int max_len(vector<vector<int>>& dp, vector<int>& nums, int i, int prev) {
+        if (i == nums.size()) {
+            return 0;
+        }
+        if (dp[i][prev + 1] != -1) {
+            return dp[i][prev + 1];
+        }
+        int take = 0, donttake = max_len(dp, nums, i + 1, prev);
+        if (prev == -1 or nums[i] > nums[prev]) {
+            take = 1 + max_len(dp, nums, i + 1, i);
+        }
+        return dp[i][prev + 1] = max(take, donttake);
+    }
+};
