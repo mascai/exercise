927bf102fefe297d96691ad6c68fff17e8dc0ae6
Alex <msumoskalenko@gmail.com>
2022-10-31 23:44:18 +0300

    Create 01_toeplitz_matrix.cpp

---
+++ 1_arrays/2D/01_toeplitz_matrix.cpp
@@ -0,0 +1,59 @@
+/* https://leetcode.com/problems/toeplitz-matrix/
+
+Given an m x n matrix, return true if the matrix is Toeplitz. Otherwise, return false.
+
+A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same elements.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
+Output: true
+Explanation:
+In the above grid, the diagonals are:
+"[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]".
+In each diagonal all elements are the same, so the answer is True.
+Example 2:
+
+
+Input: matrix = [[1,2],[2,2]]
+Output: false
+Explanation:
+The diagonal "[1, 2]" has different elements.
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 20
+0 <= matrix[i][j] <= 99
+ 
+
+Follow up:
+
+What if the matrix is stored on disk, and the memory is limited such that you can only load at most one row of the matrix into the memory at once?
+What if the matrix is so large that you can only load up a partial row into the memory at once?
+*/
+
+// O(m * n)
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (i - 1 >= 0 and j - 1 >= 0 and matrix[i - 1][j - 1] != matrix[i][j]) {
+                    return false;
+                }
+                if (i + 1 < n and j + 1 < m and matrix[i + 1][j + 1] != matrix[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
